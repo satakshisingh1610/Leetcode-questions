4a9191ada587a5590982bbed8ed0a49447a50c01
satakshisingh1610 <satakshi.singh.cs27@iilm.edu>
2026-08-27 18:35:28 +0530

    Time: 4 ms (73.07%), Space: 44.8 MB (64.25%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -0,0 +1,23 @@
+
+class Solution {
+    public void helper(TreeNode root , List<String> ans , String s){
+        if(root == null) return ;
+        if(root.left==null && root.right== null){
+            s+=root.val;
+            ans.add(s);
+            return ;
+        }
+        helper(root.left,ans,s+root.val+"->");
+        helper(root.right,ans,s+root.val+"->");
+
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        helper(root,ans,"");
+        return ans;
+        
+    }
+}
