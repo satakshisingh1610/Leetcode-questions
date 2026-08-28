27cf07b7c1ad33bf42ad1c8455399c9b88f1346c
satakshisingh1610 <satakshi.singh.cs27@iilm.edu>
2026-08-29 00:34:28 +0530

    Time: 19 ms (28.29%), Space: 44.8 MB (95.32%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int noOfPaths(TreeNode root, long targetSum){
+        if(root==null) return 0;
+        int count =0;
+        if((long)(root.val)==targetSum) count++;
+        return count + noOfPaths(root.left,targetSum-(long)(root.val))+noOfPaths(root.right,targetSum - (long)(root.val));
+            }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        int count = noOfPaths(root,(long) (targetSum));
+        count+=(pathSum(root.left,targetSum)+pathSum(root.right,targetSum));
+        return count ;
+        
+        
+    }
+}
