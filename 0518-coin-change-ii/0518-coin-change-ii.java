d631fdcf580bde4e8e8fe2132e2321ce348e7982
satakshisingh1610 <satakshi.singh.cs27@iilm.edu>
2026-09-17 04:36:50 +0530

    Time: 31 ms (13.25%), Space: 58.9 MB (5.23%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,26 +2,26 @@ class Solution {
 
     public int change(int amount, int[] coins) {
 
-        int[][] dp = new int[amount + 1][coins.length];
+        int dp[][] = new int[amount + 1][coins.length];
 
-        for(int i = 0; i <= amount; i++) {
+        for(int i = 0; i <= amount; i++){
             Arrays.fill(dp[i], -1);
         }
 
         return dfs(amount, coins, 0, dp);
     }
 
-    public int dfs(int amount, int[] coins, int index, int[][] dp) {
+    public int dfs(int amount, int[] coins, int index, int[][] dp){
 
-        if(amount == 0) {
+        if(amount == 0){
             return 1;
         }
 
-        if(amount < 0 || index == coins.length) {
+        if(amount < 0 || index == coins.length){
             return 0;
         }
 
-        if(dp[amount][index] != -1) {
+        if(dp[amount][index] != -1){
             return dp[amount][index];
         }
 
