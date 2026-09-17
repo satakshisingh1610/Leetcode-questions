83f4a8bb0f5180041411718fb396dd085f10adc8
satakshisingh1610 <satakshi.singh.cs27@iilm.edu>
2026-09-17 21:04:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (88.66%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,37 +1,27 @@
 class Solution {
-
     public int combinationSum4(int[] nums, int target) {
-
-        int[] dp = new int[target + 1];
-
-        Arrays.fill(dp, -1);
+        int [] dp = new int [target+1];
+        Arrays.fill(dp,-1);
         dp[0]=0;
+        return dfs(target,nums,dp);
 
-        return dfs(target, nums, dp);
     }
-
-    public int dfs(int target, int[] nums, int[] dp) {
-
-        if(target == 0) {
+    public int dfs(int target,int [] nums,int [] dp){
+        int sum=0;
+        if(target==0){
             return 1;
         }
-
-        if(target < 0) {
+        if(target<0){
             return 0;
         }
-
-        if(dp[target] != -1) {
+        if(dp[target]!=-1){
             return dp[target];
         }
-
-        int sum = 0;
-
-        for(int num : nums) {
-            sum += dfs(target - num, nums, dp);
+        for(int num:nums){
+            sum+= dfs(target-num,nums,dp);
         }
-
-        dp[target] = sum;
-
+        dp[target]= sum;
         return dp[target];
+
     }
 }
